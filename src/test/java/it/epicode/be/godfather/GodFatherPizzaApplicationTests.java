327b7a92a1fa714a79416db64ee4662ace83d1de
Andrea Ruzittu <ruzittu.andrea97@gmail.com>
2023-07-25 15:01:19 +0200

    upDate

--- src/test/java/it/epicode/be/godfather/GodFatherPizzaApplicationTests.java
+++ src/test/java/it/epicode/be/godfather/GodFatherPizzaApplicationTests.java
@@ -1,10 +1,10 @@
-package andrearuzittu.Epicodeitjava4D2;
+package it.epicode.be.godfather;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EpicodeItJava4D2ApplicationTests {
+class GodFatherPizzaApplicationTests {
 
 	@Test
 	void contextLoads() {
